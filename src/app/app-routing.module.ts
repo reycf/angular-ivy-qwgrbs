57e05d0a877118e87bf7cf84a6a2ac041d8be018
Charles Francois Rey <charlesfrancois.rey@epfl.ch>
2021-03-15 16:05:44 +0100

    bla2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,36 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AccreditationFormComponent } from './accreditation-form/accreditation-form.component';
-import { AccreditationFormEditComponent } from './accreditation-form-edit/accreditation-form-edit.component';
-import { AccreditationComponent } from './accreditation/accreditation.component';
-import { ComponentsComponent } from './components/components.component';
-import { VueEnsembleComponent } from './vue-ensemble/vue-ensemble.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AccreditationFormComponent } from "./accreditation-form/accreditation-form.component";
+import { AccreditationFormEditComponent } from "./accreditation-form-edit/accreditation-form-edit.component";
+import { AccreditationComponent } from "./accreditation/accreditation.component";
+import { ComponentsComponent } from "./components/components.component";
+import { VueEnsembleComponent } from "./vue-ensemble/vue-ensemble.component";
 
-const routes: Routes = [{
-  path: '',
-  component: ComponentsComponent
-}, {
-  path: 'vue-ensemble',
-  component: VueEnsembleComponent
-}, {
-  path: 'accreditation',
-  component: AccreditationComponent
-}, {
-  path: 'accreditation-form',
-  component: AccreditationFormComponent
-}, {
-  path: 'accreditation-form-edit',
-  component: AccreditationFormEditComponent
-}];
+const routes: Routes = [
+  {
+    path: "",
+    component: ComponentsComponent
+  },
+  {
+    path: "vue-ensemble",
+    component: VueEnsembleComponent
+  },
+  {
+    path: "accreditation",
+    component: AccreditationComponent
+  },
+  {
+    path: "accreditation-form",
+    component: AccreditationFormComponent
+  },
+  {
+    path: "accreditation-form-edit",
+    component: AccreditationFormEditComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
